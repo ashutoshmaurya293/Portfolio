e6d948c9d03f0025047c536f53bc2ad78834b7fb
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2024-02-02 21:57:35 +0530

    menu button added

--- src/conponents/Navbar/Navbar.jsx
+++ src/conponents/Navbar/Navbar.jsx
@@ -1,20 +1,26 @@
 import React from "react";
-import './Navbar.css'
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav>
-    <div className="container">
-      <div className="logo">Logo</div>
-      <div className="sideLogo">
-        <ul>
-          <li>Projects</li>
-          <li>Contect me</li>
-          <li>About Me</li>
-          <li>something</li>
-        </ul>
+      <div className="container">
+      <input id="checkbox" type="checkbox"/>
+    <label class="toggle" for="checkbox">
+        <div id="bar1" class="bars"></div>
+        <div id="bar2" class="bars"></div>
+        <div id="bar3" class="bars"></div>
+    </label>
+        <div className="logo">Logo</div>
+        <div className="sideLogo">
+          <ul>
+            <li>Projects</li>
+            <li>Contect me</li>
+            <li>About Me</li>
+            <li>something</li>
+          </ul>
+        </div>
       </div>
-    </div>
     </nav>
   );
 };
