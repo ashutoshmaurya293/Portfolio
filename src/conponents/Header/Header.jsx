f0bac04ba2de2a404ccafc1017b64e35156d7d48
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2024-02-06 20:37:42 +0530

    header fixed

--- src/conponents/Header/Header.jsx
+++ src/conponents/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import Drower from "../Drower/Drower";
+import "./Header.css"
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
@@ -10,7 +11,7 @@ const Header = () => {
     setcheak(!cheak);
   };
   return (
-    <div>
+    <div className="fullNav">
      <Drower toggle={toggle} OnToggle={OnToggle} />
       <Navbar OnToggle={OnToggle} cheak={cheak} setcheak={setcheak} />
     </div>
