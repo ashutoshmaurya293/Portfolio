9e129d3905daa51d6edda5d26b3a388b46b44118
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2024-02-07 18:41:20 +0530

    Drower completed

--- src/conponents/Drower/Drower.jsx
+++ src/conponents/Drower/Drower.jsx
@@ -1,18 +1,50 @@
 import React from "react";
 import styled from "styled-components";
 const Drawer = ({ toggle, OnToggle }) => {
+  const svg = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+      />
+    </svg>
+  );
   return (
     <>
       {toggle && <Backdrop />}
       <SDrawer toggle={toggle}>
-       <List>
-       <ul>
-          <li>Projects</li>
-          <li>Contect me</li>
-          <li>About Me</li>
-          <li>Something</li>
-        </ul>
-       </List>
+        <List>
+          <ul>
+            <li>
+              Home
+              {svg}
+            </li>
+            <hr />
+            <li>
+              Projects
+              {svg}
+            </li>
+            <hr />
+            <li>
+              Contect me
+              {svg}
+            </li>
+            <hr />
+            <li>
+              About Me
+              {svg}
+            </li>
+            <hr />
+          </ul>
+        </List>
       </SDrawer>
     </>
   );
@@ -24,12 +56,30 @@ const List = styled.div`
   position: absolute;
   top: 90px;
   left: 35px;
-  ul>li{
+
+  ul {
+    width: 160px;
+    /* background-color: red; */
+  }
+  li svg {
+    height: 19px;
+  }
+  ul > li {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
     list-style: none;
     padding: 2rem;
     font-size: 2.2rem;
+    width: 100%;
+    background-color: #6f07f6;
+    color: #fff;
+    &:hover {
+      background-color: #00000036;
+      color: #fff;
+    }
   }
-`
+`;
 const Backdrop = styled.div`
   height: 100%;
   width: 100%;
@@ -47,8 +97,9 @@ const SDrawer = styled.div`
   top: 0;
   height: 100vh;
   width: 60%;
-  background-color: white;
   transition: 0.3s ease;
+  background-color: #6f07f6;
+  color: #fff;
 
   transform: translateX(${(props) => (props.toggle ? "0" : "-100%")});
 `;
